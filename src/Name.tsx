2ebaf9c3279c54fab8569a8ba9fb510068c2978b
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2023-11-03 21:32:59 -0400

    save

--- src/Name.tsx
+++ src/Name.tsx
@@ -1,33 +1,39 @@
 import React, { useEffect, useRef, useState } from "react";
 import anime from "animejs";
+import { Name1nvisibilia, Nameiiiinvy, Nametaowo, NameBobbyTao, NameJiadiTao } from "./assets/nameSvgs";
 
-// const names = ["1nvisibilia", "IIIInvy", "taowo", "Bobby Tao", "Jiadi Tao"];
-const names = ["1nvisibilia"];
+const names = [Name1nvisibilia, Nameiiiinvy, Nametaowo, NameBobbyTao, NameJiadiTao];
 
 export default function Name() {
-    const myName = useRef<HTMLDivElement>(null);
-    const scale = { value: 0 };
-    const [curName, setCurName] = useState(names[0]);
+    const [idx, setIdx] = useState(1);
 
-    const switchName = async () => {
-        for (let i = 0; i < names.length; ++i) {
+    const toggle = async () => {
+        // while (true) {
             anime({
-                targets: scale,
-                value: 100,
-                round: 1,
-                duration: 500,
-                easing: 'linear',
-                update: function() {
-                    console.log(scale.value);
-                    myName.current && (myName.current.style.scale = `1 ${scale.value / 100}`);
-                }
-              });
-            await new Promise(r => setTimeout(r, 500));
-            setCurName(names[i]);
-        }
-    };
+                targets: 'path',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                easing: 'easeInOutSine',
+                duration: 800,
+                delay: function (el, i) { return i * 80 },
+                direction: 'normal',
+            });
+            await new Promise(r => setTimeout(r, 4000));
+            anime({
+                targets: 'path',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                easing: 'easeInOutSine',
+                duration: 800,
+                delay: function (el, i) { return i * 80 },
+                direction: 'reverse',
+            });
+            await new Promise(r => setTimeout(r, 3000));
+            setIdx((idx + 1) % names.length);
+        // }
+    }
 
-    useEffect(() => { switchName(); }, []);
+    useEffect(() => {
+        toggle();
+    }, []);
 
-    return <div ref={myName} className="font-sans Roboto text-9xl font-thin">{curName}</div>;
+    return <div id="myname">{names[idx]()}</div>
 }
