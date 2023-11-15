fb6a7118ba5f8105e5eb8f30ee9ceee489e340a0
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2023-11-15 15:31:53 -0500

    ASDF

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { dependencies } from "./package.json";
+
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ...renderChunks(dependencies)
+        }
+      }
+    }
+  }
 })
