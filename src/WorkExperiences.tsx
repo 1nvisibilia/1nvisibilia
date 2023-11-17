b17b6bc24ca67a14e26e4b0b314fc6f5812d076b
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2023-11-17 14:21:05 -0500

    Asdf

--- src/WorkExperiences.tsx
+++ src/WorkExperiences.tsx
@@ -4,43 +4,47 @@ import Observable from "./utils/Observable";
 import { Tooltip } from "@mui/material";
 import anime from "animejs";
 
+function animationSection(id: string) {
+    anime.timeline({})
+        .add({
+            targets: `#${id} .companylogo img`,
+            scale: [0, 1],
+            rotate: "3turn",
+            duration: 500
+        })
+        .add({
+            targets: `#${id} .workinfo > div`,
+            translateX: [300, 0],
+            duration: 200,
+            delay: anime.stagger(100),
+            opacity: [0, 1],
+            easing: "easeInOutQuad"
+        })
+        .add({
+            targets: `#${id} .workexplinker`,
+            translateY: [-100, 0],
+            opacity: [0, 1],
+            easing: "easeInOutQuad",
+            duration: 200
+        })
+        .add({
+            targets: `#${id} .workexperiencerow ul li`,
+            translateX: [-100, 0],
+            delay: anime.stagger(100),
+            opacity: [0, 1]
+        });
+}
+
 function NimbleExp() {
     useEffect(() => {
-        anime.timeline({})
-            .add({
-                targets: ".companylogo img",
-                scale: [0, 1],
-                rotate: "3turn",
-                duration: 500
-            })
-            .add({
-                targets: ".workinfo > div",
-                translateX: [300, 0],
-                duration: 200,
-                delay: anime.stagger(100),
-                opacity: [0, 1],
-                easing: "easeInOutQuad"
-            })
-            .add({
-                targets: ".workexplinker",
-                translateY: [-100, 0],
-                opacity: [0, 1],
-                easing: "easeInOutQuad",
-                duration: 200
-            })
-            .add({
-                targets: ".workexperiencerow ul li",
-                translateX: [-100, 0],
-                delay: anime.stagger(100),
-                opacity: [0, 1]
-            });
+        animationSection("nimblerx");
     }, []);
 
-    return <div style={{ display: "flex", flexDirection: "column", margin: "4em 0" }}>
+    return <div id="nimblerx">
         <div className="workexperiencerow">
             <div className="companylogo">
                 <Tooltip arrow title="NimbleRx" placement="left">
-                    <img width={100} src="./nimblerx.jpg"></img>
+                    <img width={100} src="./nimblerx.jpg" style={{ borderRadius: "50%" }}></img>
                 </Tooltip>
             </div>
             <div className="workinfo">
@@ -73,14 +77,58 @@ function NimbleExp() {
     </div>;
 }
 
+function VoiceFormExp() {
+    useEffect(() => {
+        animationSection("voiceform");
+    }, []);
+
+    return <div id="voiceform">
+        <div className="workexperiencerow">
+            <div className="companylogo">
+                <Tooltip arrow title="NimbleRx" placement="left">
+                    <img width={100} src="./voiceform.jpg" style={{ borderRadius: "50%" }}></img>
+                </Tooltip>
+            </div>
+            <div className="workinfo">
+                <div style={{ fontWeight: "bold" }}>
+                    <div>Full-stack Engineering Intern @ NimbleRx</div>
+                    <div><em>Sept. 2022 - Dec. 2022</em></div>
+                </div>
+                <div>
+                    <div><em>VueJS • Laravel • GCP • MySQL</em></div>
+                    <div><em>Toronto, Ontario, Canada</em></div>
+                </div>
+            </div>
+        </div>
+        <div className="workexperiencerow">
+            <div style={{ minWidth: "164px" }}>
+                <div className="workexplinker"></div>
+            </div>
+            <div>
+                <ul>
+                    <li>Reduced 50% of manual releasing process by setting up GitHub Action pipelines
+                        to build and upload static web embeds to the internal CDN.</li>
+                    <li>Sped up the survey submission speed by 10 times with Google's
+                        speech-to-text API service to dispatch and perform asynchronous
+                        audio data processing events through Laravel's job queues.</li>
+                    <li>and more...</li>
+                </ul>
+            </div>
+        </div>
+    </div>;
+}
+
 export default function WorkExperiences() {
     return <div style={{}}>
         <Observable threshold={1.0}>
             <SectionTitle id="workexperience" titleText="「 Whom I've worked 」" />
         </Observable>
 
-        <Observable threshold={0.5}>
+        <Observable threshold={0.7}>
             <NimbleExp />
+        </Observable>
+        <Observable threshold={0.7}>
+            <VoiceFormExp />
         </Observable>
     </div>
 }
