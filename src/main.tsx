78679149601438f99efed68e2891d5b487d4f39b
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2023-11-22 22:42:54 -0500

    ASDF

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if (typeof screen.orientation === "string" && screen.orientation !== "undefined") {
+  alert("This site is best viewed on desktop browser, or mobile with desktop view!");
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
